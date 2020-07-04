52d133e45ef9daec07b2232b6a62e84ddf761f82
Doug Wright <mrdougwright@gmail.com>
2020-07-04 07:44:02 -0700

    prettier; update readme

--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 ### Hyperapp demo
 
 Learn more here https://hyperapp.dev/
+
+Install & Run using webpack
+
+```
+npm install
+npm run build
+```
+
+Then open `dist/index.html` in browser.
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import { h, app } from "hyperapp"
+import { h, app } from 'hyperapp'
 
 app({
   init: 0,
-  view: state =>
-    h("div", {}, [
-      h("h1", {}, state),
-      h("button", { onclick: state => state - 1 }, "subtract"),
-      h("button", { onclick: state => state + 1 }, "add")
+  view: (state) =>
+    h('div', {}, [
+      h('h1', {}, state),
+      h('button', { onclick: (state) => state - 1 }, 'subtract'),
+      h('button', { onclick: (state) => state + 1 }, 'add'),
     ]),
-  node: document.getElementById("app")
+  node: document.getElementById('app'),
 })
